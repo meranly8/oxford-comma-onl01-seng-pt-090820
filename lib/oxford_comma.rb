051b8946d0ec744101247d048001a2d37156119c
Michelle Ranly <meranly8@gmail.com>
2020-09-14 23:40:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size >= 3 
+    last_item = array.pop
+    and_last_item = "and #{last_item}"
+    array << and_last_item
+    array.join(", ")
+  else
+    array.join(" and ")  
+  end
 end
+
+
+parks = ["Glacier", "Yellowstone", "Yosemite", "Mammoth"]
+
+oxford_comma(parks)
